f80a652410a50b12f3b1b1e3edbdbed0a22ca963
sourav0299 <105085605+sourav0299@users.noreply.github.com>
2025-01-04 20:51:21 +0530

    Feature: Business Details page UI

--- app/business-settings/page.tsx
+++ app/business-settings/page.tsx
@@ -1,9 +1,81 @@
 import React from 'react'
 
+const PhotoIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path
+      d="M3 16L8 9L14 15.5M20.5 18L16 13L11.7143 19M14 10H14.01M4 19H20C20.5523 19 21 18.5523 21 18V6C21 5.44772 20.5523 5 20 5H4C3.44772 5 3 5.44772 3 6V18C3 18.5523 3.44772 19 4 19Z"
+      stroke="#1EB386"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
 const BusinessSettings = () => {
   return (
-    <div className=" bg-universal_gray_background h-[41.9rem]">business settings</div>
-  )
+    <div className=" bg-universal_gray_background h-[41.9rem]">
+      <div className="px-6 gap-3">
+        <div className="py-6 gap-1">
+          <div className="text-3xl font-semibold text-business_settings_black_text">
+            Your Business Details
+          </div>
+          <div className="text-lg font-medium text-business_settings_gray_text">
+            An Overview of all your transactions over the year.
+          </div>
+        </div>
+        <div className="rounded-lg bg-universal_white_background flex flex-col p-6 h-auto">
+          <div className="flex gap-3">
+            <div className="flex flex-col items-center justify-center p-4 w-[188px] h-[188px] rounded-lg border-dashed border border-business_settings_gray_border">
+              <div className="">
+                <PhotoIcon />
+              </div>
+              <div className="text-xs font-medium text-sidebar_green_button_background">
+                + Upload Image
+              </div>
+            </div>
+            <div className="flex flex-col w-full gap-6">
+              <div className="p-5 bg-universal_gray_background rounded-lg gap-1">
+                <div className="bg-transparent w-full text-xs text-sidebar_black_text">
+                  Business Name
+                </div>
+                <input
+                  type="text"
+                  className="bg-transparent border border-dashed w-full h-8 rounded-[4px] focus:outline-none p-1"
+                />
+              </div>
+              <div className="flex w-full gap-3">
+                <div className="flex flex-col w-full bg-universal_gray_background p-5 rounded-lg gap-1">
+                  <div className="bg-transparent w-full text-xs text-sidebar_black_text">
+                    Buisness Type
+                  </div>
+                  <input
+                    type="text"
+                    className="bg-transparent border border-dashed w-full h-8 rounded-[4px] focus:outline-none p-1"
+                  />
+                </div>
+                <div className="flex flex-col w-full bg-universal_gray_background p-5 rounded-lg gap-1">
+                  <div className="bg-transparent w-full text-xs text-sidebar_black_text">
+                    Business Registration Type
+                  </div>
+                  <input
+                    type="text"
+                    className="bg-transparent border border-dashed w-full h-8 rounded-[4px] focus:outline-none p-1"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BusinessSettings
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ export default {
     extend: {
       colors: {
         universal_gray_background: "var(--Neutral-Background, #FAFAFA)",
+        universal_white_background: "#ffffff",
         headerbar_gray_border: "1px solid var(--Neutral-Black-Black-50, #F0F1F3)",
         sidebar_white_background: "#fffff",
         sidebar_black_text: "#667085",
@@ -17,6 +18,9 @@ export default {
         sidebar_gray_border: "#F0F1F3",
         sidebar_bottom_gray_background: "#F7F7F7",
         sidebar_bottom_red_logout_text_color: "#FF7A7A",
+        business_settings_black_text: "#121212",
+        business_settings_gray_text: "#7D8398",
+        business_settings_gray_border: "var(--Neutral-500, #667877)",
       },
     },
   },
