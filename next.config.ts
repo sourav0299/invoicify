14b755be997fc7dd40810980bca8e3cd4db8dc1e
sourav kumar <sourav2000kumar07@gmail.com>
2025-03-22 19:53:07 +0530

    feat

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,6 @@ const nextConfig = {
     domains: ['img.clerk.com', 'res.cloudinary.com'],
   },
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/landing",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
